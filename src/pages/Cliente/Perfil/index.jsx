82ec1166ba78c314882e07011f25544c18323cde
Gerson Armando Bié <64958963+gersonbie@users.noreply.github.com>
2022-06-05 21:18:49 +0200

    Add logout button

--- src/pages/Cliente/Perfil/index.jsx
+++ src/pages/Cliente/Perfil/index.jsx
@@ -6,35 +6,45 @@ import { useAuth } from "../../../context";
 import { APIKit } from "../../../services/api";
 import "./perfil.css";
 
-
 const Perfil = () => {
-  const { usuario, toast } = useAuth(useAuth);
-  const [reservas, setReservas] = useState([])
+  const { usuario, toast, logout } = useAuth(useAuth);
+  const [reservas, setReservas] = useState([]);
   const fetchData = () => {
     APIKit.get(`/marcacoes?id=${usuario.id}`)
-      .then(response => setReservas(response.data))
-      .catch(() => toast.error("Erro ao Buscar usuários!"))
-  }
- 
+      .then((response) => setReservas(response.data))
+      .catch(() => toast.error("Erro ao Buscar usuários!"));
+  };
+
   useEffect(() => {
-    fetchData()
-  }, [])
+    fetchData();
+  }, []);
 
   return (
     <div className="container perfil">
       <div className="profile-card shadow">
         <div className="icon relative">
           <SellerIcon />
-          {usuario?.ativo ? <div className="enabled-circle" /> : <div className="disabled-circle" />}
+          {usuario?.ativo ? (
+            <div className="enabled-circle" />
+          ) : (
+            <div className="disabled-circle" />
+          )}
         </div>
         <div className="flex-col p-4 profile-main">
           <h3 className="mb-1 blue-text">{usuario?.nome}</h3>
           <span className="text-opaque">{usuario?.email}</span>
           <span className="text-opaque">{usuario?.contacto}</span>
+          <button
+            type="button"
+            onClick={logout}
+            className="logout ph-4 pv-1 shadow"
+          >
+            Terminar sessão
+          </button>
         </div>
         <div className="flex-col profile-footer p-4">
           <div className="flex">
-          <CustomTable header={headersAtendimento} items={reservas?.items} />
+            <CustomTable header={headersAtendimento} items={reservas?.items} />
           </div>
         </div>
       </div>
